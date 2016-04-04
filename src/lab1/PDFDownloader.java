9e5262c4efdad2050e452b66e7b3ac17d5ad1524
ryougi1 <shplankey@live.se>
2016-04-04 18:39:54 +0200

    done but not happy with the saveoptions

--- src/lab1/PDFDownloader.java
+++ src/lab1/PDFDownloader.java
@@ -1,29 +1,37 @@
 package lab1;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.swing.JFileChooser;
 
 public class PDFDownloader {
 	private URL url;
 	private static String html;
+	private static ArrayList<URL> pdfs;
 
 	public PDFDownloader(URL url) throws IOException {
 		this.url = url;
-		html = downloadHTML();
+		downloadHTML();
+		getPDFLinks();
 	}
 
-	public void downloadPDFs() {
-		ArrayList<String> pdfs = getPDFLinks();
-	}
-	
-	private String downloadHTML() throws IOException {
+	private void downloadHTML() throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
 		String s;
 		StringBuilder sb = new StringBuilder();
@@ -31,26 +39,53 @@ public class PDFDownloader {
 			sb.append(s);
 			sb.append("\n");
 		}
-		return sb.toString();
+		html = sb.toString();
 	}
 
-	private ArrayList<String> getPDFLinks() {
-		ArrayList<String> PDFlinks = new ArrayList<String>();
+	private void getPDFLinks() throws MalformedURLException {
+		pdfs = new ArrayList<URL>();
 		Pattern linkPattern = Pattern.compile("href=\"(.*?)\"");
 		Matcher linkMatcher = linkPattern.matcher(html);
 		while (linkMatcher.find()) {
 			if (linkMatcher.group(1).endsWith(".pdf")) {
-				PDFlinks.add(linkMatcher.group(1));				
+				pdfs.add(new URL(linkMatcher.group(1)));
 			}
 		}
-		
-		return PDFlinks;
+	}
+
+	public void download() throws IOException {
+		int i = 1;
+		for (URL u : pdfs) {
+			Files.copy(u.openStream(), Paths.get("file" + i + ".pdf"), StandardCopyOption.REPLACE_EXISTING);
+			i++;
+		}
+	}
+
+	public void download2() {
+		Path path = null;
+		for (URL u : pdfs) {
+			File file;
+			JFileChooser jf = new JFileChooser();
+			switch (jf.showSaveDialog(jf)) {
+			case JFileChooser.APPROVE_OPTION:
+				file = jf.getSelectedFile();
+				break;
+			default:
+				return;
+			}
+			path = Paths.get(file.getPath());
+			try {
+	    		Files.copy(u.openStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
 	}
 
 	public static void main(String[] args) throws IOException {
 		PDFDownloader d = new PDFDownloader(new URL("http://cs.lth.se/eda095/foerelaesningar/?no_cache=1"));
-		d.downloadPDFs();
+		d.download2();
 
-		// System.out.println(d.downloadHTML());
 	}
 }
